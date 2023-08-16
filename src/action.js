054e2e751e714a7464846802048c75d8b629470f
CLB <20195357+altyjp@users.noreply.github.com>
2023-08-16 19:10:03 +0900

    利用しないアクションを削除

--- src/action.js
+++ src/action.js
@@ -1,12 +1,3 @@
-
-
-export function fetchActions(payload) {
-  return {
-    type: 'GET_POST_DATA',
-    payload: payload,
-  };
-}
-
 export const GetPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await res.json();
